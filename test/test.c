76a26b0f35e2e1b5b4825edad6c9bb5d8a009ba4
Pete Batard <pete@akeo.ie>
2026-06-21 14:15:02 +0100

    reinstate bled_get_uncompressed_size()
    * Also add a BLED_EXPECT_DISK_IMAGE mode, where Bled attempts to detect the size of the compressed
      data if the archive consists of a single compressed flat MBR or BPT based disk image.

--- test/test.c
+++ test/test.c
@@ -46,7 +46,14 @@ int main(int argc, char** argv)
 
 	bled_init(0, NULL, NULL, NULL, NULL, NULL, NULL);
 
-	printf("DECOMPRESS TO BUFFER:\n");
+	printf("GET UNCOMPRESSED SIZE:\n");
+	for (i = 0; i < ARRAYSIZE(test_files); i++) {
+		sprintf_s(src, sizeof(src), "%s%s.%s", BASE_PATH, BASE_FILE, test_files[i].ext);
+		r = bled_get_uncompressed_size(src, test_files[i].type);
+		printf("  %s:\t%" PRIi64 "\n", test_files[i].ext, r);
+	}
+
+	printf("\nDECOMPRESS TO BUFFER:\n");
 	buffer = malloc(BUFSIZE);
 	for (i = 0; i < ARRAYSIZE(test_files); i++) {
 		sprintf_s(src, sizeof(src), "%s%s.%s", BASE_PATH, BASE_FILE, test_files[i].ext);
